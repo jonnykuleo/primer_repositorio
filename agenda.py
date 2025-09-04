6591b745740b5d612a9708e01cbddfe550fb7700
martin <martin.ubeda@gmail.com>
2025-09-04 14:17:53 -0400

    agrego funcion para buscar contactos

--- agenda.py
+++ agenda.py
@@ -4,10 +4,20 @@ contacto = []
 def agregar_contacto(nombre,telefono):
     contacto.append({"nombre": nombre, "telefono":telefono})
 
+def buscar_contacto(nombre:)
+    for c in contactos:
+    if c['nombre'].lower() == nombre.lower():
+        print(f"caontactos encontrado: {c["nombre"]} - {c["telefono"]}")
+        return
+    print('contacto no encontrado')
+    
+
 def lista_contactos():
     print("\nlista de contactos: ")
     for c in contactos:
         print(f"{c["nombre"]} - {c["telefono"]}")
+
+lista_contactos()
 
 
 nombre = input("ingresael nombre :")
@@ -15,4 +25,3 @@ telefono = input("ingrese el telefono: ")
 agregar_contacto(nombre,telefono)
 print("Contacto agregado")
 
-lista_contactos()
